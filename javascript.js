6ea1769ab617518b1f6375c6ac4b79141b9a5aa0
SedgeDuck <wren.gray.pdx@gmail.com>
2024-06-11 18:36:56 -0700

    some stuff

--- javascript.js
+++ javascript.js
@@ -1,3 +1,32 @@
+let first = prompt("Enter a number: ");
+let second = prompt("Enter another number: ");
+let str = prompt("Enter a string: ");
+
+let result1 = add7(first);
+console.log(result1);
+let result2 = multiply(first, second);
+console.log(result2);
+let result3 = capitalize(str);
+console.log(result3);
+let result4 = lastLetter(str);
+console.log(result4);
+
+function add7(num1) {
+  return Number(num1) + 7;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function capitalize(text) {
+  return text.at(0).toUpperCase() + text.slice(1).toLowerCase();
+}
+
+function lastLetter(text) {
+  return text.at(-1);
+}
+
 /*let ask = (question, yes, no) => (confirm(question) ? yes : no);
 
 ask(
